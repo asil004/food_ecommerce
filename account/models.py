99ab7d90fb8b4c6d71a88e515f5c1b6f9bc0bdf0
Xumoyun112 <xumoyunelmurodov03@gmail.com>
2024-03-16 21:41:14 +0500

    added models

--- account/models.py
+++ account/models.py
@@ -3,5 +3,9 @@ from base.models import TimeStampModel
 
 
 class User(TimeStampModel):
-    pass
-# test
+    first_name = models.CharField(max_length=150)
+    last_name = models.CharField(max_length=150)
+    address = models.TextField()
+    email = models.EmailField()
+    phone_number = models.IntegerField(max_length=20)
+    password = models.CharField(max_length=30)
--- banners/models.py
+++ banners/models.py
@@ -1,3 +1,9 @@
 from django.db import models
+from base.models import TimeStampModel
+from products.models import Product
 
-# Create your models here.
+
+class Banners(TimeStampModel):
+    product = models.ForeignKey(Product, on_delete=models.CASCADE, blank=True)
+    image = models.ImageField(upload_to="/banners/images")
+    text = models.TextField()
