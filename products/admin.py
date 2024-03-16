78817f5d41b026137873fa7c3d9bd8d448463f44
Shukurov Sardor <sardorshukurov2003S@gmail.com>
2024-03-16 20:41:41 +0500

    Discountni admin panelga qoshdim

--- products/admin.py
+++ products/admin.py
@@ -1,8 +1,9 @@
 from django.contrib import admin
-from .models import Product, Images, Color, Size, Stars
+from .models import Product, Images, Color, Size, Stars,Discount
 
 admin.site.register(Product)
 admin.site.register(Images)
 admin.site.register(Color)
 admin.site.register(Size)
 admin.site.register(Stars)
+admin.site.register(Discount)
