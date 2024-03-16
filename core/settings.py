6b6b69141a15f6ce984d1a338579a43709fa33c3
Shukurov Sardor <sardorshukurov2003S@gmail.com>
2024-03-16 17:53:56 +0500

    category model va base app qoshdim

--- core/settings.py
+++ core/settings.py
@@ -37,6 +37,10 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'base.apps.BaseConfig'
+    'category.apps.CategoryConfig'
+    'product.apps.ProductConfig'
+    'wishlist.apps.WishlistConfig'
 ]
 
 MIDDLEWARE = [
