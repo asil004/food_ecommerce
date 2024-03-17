d1b358760b1e1212d1c7073bc7fe50d0040b7e74
Rustam2900 <139065064+Rustam2900@users.noreply.github.com>
2024-03-17 13:18:09 +0500

    views

--- categories/models.py
+++ categories/models.py
@@ -1,16 +1,15 @@
 from django.db import models
-from mptt.models import TreeForeignKey,MPTTModel
+from mptt.models import TreeForeignKey, MPTTModel
 from base.models import TimeStampModel
 
 
-class Category(MPTTModel,TimeStampModel):
+class Category(MPTTModel, TimeStampModel):
     category_name = models.CharField(max_length=50, unique=True)
     parent = TreeForeignKey('self', on_delete=models.CASCADE, null=True, blank=True, related_name='cate_product')
 
     @classmethod
     def publish(cls):
         return cls.objects.all()
-
 
     def __str__(self):
         return self.category_name
--- checkout/views.py
+++ checkout/views.py
@@ -1,3 +1,3 @@
 from django.shortcuts import render
+from .models import Checkout
 
-# Create your views here.
