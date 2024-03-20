660c18941e0e053e4a3a0fabb5e4626f21474a2d
Rustam2900 <139065064+Rustam2900@users.noreply.github.com>
2024-03-21 00:09:29 +0500

    contactga serializers va urls fayl yozildi va modelsga o'zgartirish kiritildi

--- contact/models.py
+++ contact/models.py
@@ -3,10 +3,10 @@ from base.models import TimeStampModel
 
 
 class Contact(TimeStampModel):
-    first_name = models.CharField(max_length=100)
-    email = models.EmailField(max_length=30)
-    phone = models.CharField(max_length=20)
-    message = models.TextField()
+    first_name = models.CharField(max_length=100, blank=True, null=True)
+    email = models.EmailField(max_length=30, blank=True, null=True)
+    phone = models.CharField(max_length=20, blank=True, null=True)
+    message = models.TextField(blank=True, null=True)
 
     def __str__(self):
         return self.first_name
--- core/urls.py
+++ core/urls.py
@@ -35,6 +35,7 @@ schema_view = get_schema_view(
 urlpatterns = [
     path('admin/', admin.site.urls),
     path("api/v1/", include("account.urls")),
+    path("api/v1/", include("contact.urls")),
     path('docs/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
 
 ]
