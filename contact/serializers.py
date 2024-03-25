a309e11b143968745262d32d74bbf9f6b30507f7
Rustam2900 <139065064+Rustam2900@users.noreply.github.com>
2024-03-25 17:52:59 +0500

    oredrga serializers va viws fayl yozildi

--- contact/serializers.py
+++ contact/serializers.py
@@ -3,9 +3,7 @@ from rest_framework import serializers
 
 
 class ContactSerializers(serializers.Serializer):
-    # first_name = serializers.CharField(max_length=255)
-    # email = serializers.EmailField()
-    # phone = serializers.CharField(max_length=20)
-    # message = serializers.CharField(max_length=255)
-    model=Contact
-    fields="__all__"
+    first_name = serializers.CharField(max_length=255)
+    email = serializers.EmailField()
+    phone = serializers.CharField(max_length=20)
+    message = serializers.CharField(max_length=255)
