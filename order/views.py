b586854e48697117c0ea698f841d594b39bc2bf4
Rustam2900 <139065064+Rustam2900@users.noreply.github.com>
2024-03-25 18:06:44 +0500

    oredrga serializers va viws fayl yozildi

--- order/views.py
+++ order/views.py
@@ -1,9 +1,16 @@
-from django.shortcuts import render
+from rest_framework import generics
+from rest_framework.permissions import IsAuthenticated
 from .models import Order
 from .serializers import OrderSerializers
-from rest_framework import generics
 
 
-class OrderListAPIView(generics.ListAPIView):
+class OrderListCreateView(generics.ListCreateAPIView):
     queryset = Order.objects.all()
     serializer_class = OrderSerializers
+    permission_classes = [IsAuthenticated]
+
+
+class OrderRetrieveUpdateDestroyView(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Order.objects.all()
+    serializer_class = OrderSerializers
+    permission_classes = [IsAuthenticated]
