674122e4e21804cc4fc15f97e3793c2fd95bc9ae
asil004 <72306088+asil004@users.noreply.github.com>
2024-03-28 19:57:47 +0500

    adding product chekout

--- checkout/admin.py
+++ checkout/admin.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
-from .models import CheckoutBasket, CheckoutProduct, BillingDetails
+from .models import CheckoutBasket, ProductCheckout, BillingDetails
 
 admin.site.register(CheckoutBasket)
-admin.site.register(CheckoutProduct)
+admin.site.register(ProductCheckout)
 admin.site.register(BillingDetails)
--- checkout/models.py
+++ checkout/models.py
@@ -44,13 +44,11 @@ class CheckoutBasket(Checkout):
         return str(self.id)
 
 
-class CheckoutProduct(Checkout):
-    product = models.ForeignKey(Product, related_name='product_checkout', on_delete=models.CASCADE),
-    color = models.ForeignKey(Color, on_delete=models.CASCADE, null=True, blank=True)
-    size = models.ForeignKey(Size, on_delete=models.CASCADE, null=True, blank=True)
+class ProductCheckout(Checkout):
+    product = models.ForeignKey(Product, on_delete=models.CASCADE, related_name='product_checkout')
     quantity = models.PositiveSmallIntegerField(default=0)
     total_sum = models.DecimalField(max_digits=10, decimal_places=2, default=0)
-    billing_details = models.ForeignKey(BillingDetails, on_delete=models.CASCADE, related_name='CheckoutProduct')
+    billing_details = models.ForeignKey(BillingDetails, on_delete=models.CASCADE, related_name='Checkoutbilling')
     account = models.ForeignKey(User, on_delete=models.CASCADE, related_name='CheckoutProduct')
 
     def __str__(self):
