c277b3dc3e5ae577b6cfd814196670d717d05e54
Shukurov Sardor <sardorshukurov2003S@gmail.com>
2024-03-28 15:15:01 +0500

    checkout

--- checkout/serializers.py
+++ checkout/serializers.py
@@ -2,6 +2,7 @@ from basket.models import ProductBasket
 from basket.serializers import ProductBasketSerializer
 from .models import Checkout, BillingDetails, bank_card
 from rest_framework import serializers
+from products.models import *
 
 
 class BillingDetailsSerializers(serializers.ModelSerializer):
@@ -28,3 +29,45 @@ class MyOrdersSerializer(serializers.ModelSerializer):
     class Meta:
         model = Checkout
         exclude = ['created_at', 'updated_at', 'cupon_code', 'card_number', 'card_date', 'account']
+
+
+class ImageSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Images
+        exclude = ['created_at', 'updated_at']
+
+
+class StarsSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Stars
+        exclude = ['created_at', 'updated_at']
+
+
+class ColorsSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Color
+        exclude = ['created_at', 'updated_at']
+
+
+class SizesSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Size
+        exclude = ['created_at', 'updated_at']
+
+class DiscountsSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Discount
+        exclude = ['created_at', 'updated_at']
+
+
+class billingDetailscheckutSerializers(serializers.ModelSerializer):
+    images = ImageSerializer(many=True)
+    color = ColorsSerializer(many=True)
+    size = SizesSerializer(many=True)
+    stars = StarsSerializer(many=True)
+
+
+    class Meta:
+        model = Product
+        fields = ["id", "name", "slug", "price", "quantity", "description", "category", "color", "size",
+                  "images", "stars", "discount"]
--- products/models.py
+++ products/models.py
@@ -4,22 +4,6 @@ from base.models import TimeStampModel
 from categories.models import Category
 
 
-class Color(TimeStampModel):
-    name = models.CharField(max_length=20, unique=True)
-    slug = models.SlugField(max_length=20, unique=True)
-
-    def __str__(self):
-        return self.name
-
-
-class Size(TimeStampModel):
-    name = models.CharField(max_length=20, unique=True)
-    slug = models.SlugField(max_length=20, unique=True)
-
-    def __str__(self):
-        return self.name
-
-
 class Discount(TimeStampModel):
     dis_percentage = models.IntegerField(blank=True, null=True)
     end_time = models.DateTimeField()
@@ -29,18 +13,35 @@ class Discount(TimeStampModel):
 
 
 class Product(TimeStampModel):
-    name = models.CharField(max_length=255, unique=True)
+    name = models.CharField(max_length=255)
     slug = models.SlugField(max_length=100, unique=True)
     price = models.DecimalField(max_digits=10, decimal_places=2)
     quantity = models.IntegerField(default=0)
     description = models.TextField()
     category = models.ForeignKey(Category, on_delete=models.PROTECT, related_name='products')
-    color = models.ForeignKey(Color, on_delete=models.PROTECT, null=True, blank=True)
-    size = models.ForeignKey(Size, on_delete=models.PROTECT, null=True, blank=True)
     discount = models.ForeignKey(Discount, on_delete=models.PROTECT, null=True, blank=True)
 
+
     def __str__(self):
-        return self.name
+        return self.slug
+
+
+class Color(TimeStampModel):
+    product = models.ForeignKey(Product, on_delete=models.CASCADE, related_name='color')
+    color = models.CharField(max_length=20)
+    slug = models.SlugField(max_length=20, unique=True)
+
+    def __str__(self):
+        return self.slug
+
+
+class Size(TimeStampModel):
+    product = models.ForeignKey(Product, on_delete=models.CASCADE, related_name='size')
+    size = models.CharField(max_length=20)
+    slug = models.SlugField(max_length=20, unique=True)
+
+    def __str__(self):
+        return self.slug
 
 
 class Images(TimeStampModel):
