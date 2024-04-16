5001cf429d58da8431321b4c4af2603afce7d189
Asilbek Tolibov <72306088+asil004@users.noreply.github.com>
2024-04-16 15:47:21 +0500

    Update settings.py

--- core/settings.py
+++ core/settings.py
@@ -61,8 +61,8 @@ REST_FRAMEWORK = {
 }
 
 SIMPLE_JWT = {
-    "ACCESS_TOKEN_LIFETIME": timedelta(days=1),
-    "REFRESH_TOKEN_LIFETIME": timedelta(days=2),
+    "ACCESS_TOKEN_LIFETIME": timedelta(days=7),
+    "REFRESH_TOKEN_LIFETIME": timedelta(days=8),
     "ROTATE_REFRESH_TOKENS": True,
     "BLACKLIST_AFTER_ROTATION": True,
     "UPDATE_LAST_LOGIN": False,
